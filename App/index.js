ea69b195495f0678b0856ae923cd37cb26c7a286
zachee <zacheeishimwe@gmail.com>
2020-06-03 09:32:22 +0200

    creating options

---
+++ App/index.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import Options from "./screens/Options";
+export default ()=><Options/>
---
+++ App/screens/Options.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, TouchableOpacity, Text, SafeAreaView, StyleSheet } from 'react-native';
+import colors from '../constants/colors';
+const styles = StyleSheet.create({
+	row: {
+		paddingHorizontal: 20,
+		paddingVertical: 16
+	},
+	text: {
+		fontSize: 16,
+		color: colors.text
+	},
+	separator: {
+		backgroundColor: colors.border,
+		height: StyleSheet.hairlineWidth,
+		marginLeft: 20
+	}
+});
+
+export default () => {
+	return (
+		<SafeAreaView>
+			<TouchableOpacity style={styles.row}>
+				<Text style={styles.text}>Themes</Text>
+			</TouchableOpacity>
+
+			<View style={styles.separator} />
+
+			<TouchableOpacity style={styles.row}>
+				<Text>React Native Basics</Text>
+			</TouchableOpacity>
+
+			<View style={styles.separator} />
+
+			<TouchableOpacity style={styles.row}>
+				<Text>REACT native by examples</Text>
+			</TouchableOpacity>
+		</SafeAreaView>
+	);
+};
