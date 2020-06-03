0bca97cb91315ab5ec007a621c705fcef1fa429d
zachee <zacheeishimwe@gmail.com>
2020-06-03 10:25:02 +0200

    create a custom component

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,47 +1,33 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 import colors from '../constants/colors';
-const styles = StyleSheet.create({
-	row: {
-		paddingHorizontal: 20,
-		paddingVertical: 16,
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'space-between'
-	},
-	text: {
-		fontSize: 16,
-		color: colors.text
-	},
-	separator: {
-		backgroundColor: colors.border,
-		height: StyleSheet.hairlineWidth,
-		marginLeft: 20
-	}
-});
+import { RowItem, RowSeparator } from '../components/RowItem';
 
 export default () => {
 	return (
 		<SafeAreaView>
-			<TouchableOpacity style={styles.row}>
-				<Text style={styles.text}>Themes</Text>
-				<Entypo name="chevron-right" size={20} color={colors.blue} />
-			</TouchableOpacity>
+			<RowItem
+				text="Themes"
+				onPress={() => alert('tod!')}
+				rightIcon={<Entypo name="chevron-right" size={20} color={colors.blue} />}
+			/>
 
-			<View style={styles.separator} />
+			<RowSeparator />
 
-			<TouchableOpacity style={styles.row}>
-				<Text>React Native Basics</Text>
-				<Entypo name="export" size={20} color={colors.blue} />
-			</TouchableOpacity>
+			<RowItem
+				text="React Native Basics"
+				onPress={() => alert('tod!')}
+				rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+			/>
 
-			<View style={styles.separator} />
+			<RowSeparator />
 
-			<TouchableOpacity style={styles.row}>
-				<Text>REACT native by examples</Text>
-				<Entypo name="export" size={20} color={colors.blue} />
-			</TouchableOpacity>
+			<RowItem
+				text="React Native by example"
+				onPress={() => alert('tod!')}
+				rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+			/>
 		</SafeAreaView>
 	);
 };
