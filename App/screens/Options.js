2775d8e54a8532bd6710ff35f395a2443ca0bf26
zachee <zacheeishimwe@gmail.com>
2020-06-03 09:49:02 +0200

    adding icons on the screen

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, TouchableOpacity, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 import colors from '../constants/colors';
 const styles = StyleSheet.create({
 	row: {
 		paddingHorizontal: 20,
-		paddingVertical: 16
+		paddingVertical: 16,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between'
 	},
 	text: {
 		fontSize: 16,
@@ -22,18 +26,21 @@ export default () => {
 		<SafeAreaView>
 			<TouchableOpacity style={styles.row}>
 				<Text style={styles.text}>Themes</Text>
+				<Entypo name="chevron-right" size={20} color={colors.blue} />
 			</TouchableOpacity>
 
 			<View style={styles.separator} />
 
 			<TouchableOpacity style={styles.row}>
 				<Text>React Native Basics</Text>
+				<Entypo name="export" size={20} color={colors.blue} />
 			</TouchableOpacity>
 
 			<View style={styles.separator} />
 
 			<TouchableOpacity style={styles.row}>
 				<Text>REACT native by examples</Text>
+				<Entypo name="export" size={20} color={colors.blue} />
 			</TouchableOpacity>
 		</SafeAreaView>
 	);
