b7ec049edd58d7703984c7df3fa3a5c02521a8d7
zachee <zacheeishimwe@gmail.com>
2020-06-04 12:07:23 +0200

    enable screeen scroll when the keyboard is open

--- App/screens/home.js
+++ App/screens/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, StatusBar, Image, Dimensions, Text } from 'react-native';
+import { View, StyleSheet, StatusBar, Image, Dimensions, Text, ScrollView } from 'react-native';
 import { format } from 'date-fns';
 import colors from '../constants/colors';
 import { ConversionInput } from '../components/ConversionInput';
@@ -9,8 +9,10 @@ const screen = Dimensions.get('window');
 const styles = StyleSheet.create({
 	container: {
 		backgroundColor: colors.blue,
-		flex: 1,
-		justifyContent: 'center'
+		flex: 1
+	},
+	content: {
+		paddingTop: screen.height * 0.2
 	},
 	logoContainer: {
 		alignItems: 'center',
@@ -46,29 +48,33 @@ export default () => {
 	const date = new Date();
 	return (
 		<View style={styles.container}>
-			<StatusBar barStyle="light-content" backgroundColor={colors.blue} />
-
-			<View style={styles.logoContainer}>
-				<Image
-					source={require('../assets/images/background.png')}
-					style={styles.logBackground}
-					resizeMode="contain"
-				/>
-				<Image source={require('../assets/images/logo.png')} style={styles.logo} resizeMode="contain" />
-			</View>
-			<Text style={styles.textHeader}> Curreny converter</Text>
-			<ConversionInput
-				text={baseCurrency}
-				value="123"
-				onButtonPress={() => alert('todo!')}
-				onChangeText={(text) => console.log(text)}
-				keyboardType="numeric"
-			/>
-			<ConversionInput text="GDP" value="123" onButtonPress={() => alert('todo!')} editable={false} />
-			<Text style={styles.text}>
-				{`1 ${baseCurrency}= ${conversionRate} ${quoteCurrency} as of ${format(date, 'MMM do,yyy')}`}
-			</Text>
-			<Button text="Reverse Currencies" onPress={() => alert('todo!')} />
+			<ScrollView>
+				<StatusBar barStyle="light-content" backgroundColor={colors.blue} />
+				<View style={styles.content}>
+					<View style={styles.logoContainer}>
+						<Image
+							source={require('../assets/images/background.png')}
+							style={styles.logBackground}
+							resizeMode="contain"
+						/>
+						<Image source={require('../assets/images/logo.png')} style={styles.logo} resizeMode="contain" />
+					</View>
+					<Text style={styles.textHeader}> Curreny converter</Text>
+					<ConversionInput
+						text={baseCurrency}
+						value="123"
+						onButtonPress={() => alert('todo!')}
+						onChangeText={(text) => console.log(text)}
+						keyboardType="numeric"
+					/>
+					<ConversionInput text="GDP" value="123" onButtonPress={() => alert('todo!')} editable={false} />
+					<Text style={styles.text}>
+						{`1 ${baseCurrency}= ${conversionRate} ${quoteCurrency} as of ${format(date, 'MMM do,yyy')}`}
+					</Text>
+					<Button text="Reverse Currencies" onPress={() => alert('todo!')} />
+					<View style={{ height: screen.height }} />
+				</View>
+			</ScrollView>
 		</View>
 	);
 };
