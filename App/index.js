876cc6219af30ecf1b60648cab6e5e973bacbb7a
zachee <zacheeishimwe@gmail.com>
2020-06-05 15:24:25 +0200

    add api for currency rate

--- App/index.js
+++ App/index.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import Navigation from './config/Navigation';
+import { api } from './utils/api';
+
+api('/latest?base=USD')
+	.then((response) => {
+		console.log('response', response);
+	})
+	.catch((error) => {
+		console.log('error', error);
+	});
 export default () => <Navigation />;
